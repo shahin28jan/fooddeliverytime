1b68db3c5a7964e0de2b0103a37884c285e1e4fd
shahin28jan <shahinalwaz@gmail.com>
2023-04-26 00:16:49 +0530

    EDA exception.py

--- src/exception.py
+++ src/exception.py
@@ -20,12 +20,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message    
     
-'''
-if __name__=="__main__":
-    logging.info("Logging has started")
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Dicision by zero') 
-        raise CustomException(e,sys)
-'''
